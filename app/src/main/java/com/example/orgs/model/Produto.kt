d6ff2417c988a5aa79a4f8cd851a035f1f8121a2
silas2117 <silas.matsumoto@hotmail.com>
2024-10-16 11:56:15 -0300

    Start commit!

---
+++ app/src/main/java/com/example/orgs/model/Produto.kt
@@ -0,0 +1,9 @@
+package com.example.orgs.model
+
+import java.math.BigDecimal
+
+class Produto(
+    val nome: String,
+    val descricao: String,
+    val valor: BigDecimal
+)
---
+++ app/src/main/java/com/example/orgs/ui/activity/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.orgs.ui.activity
+
+import android.app.Activity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.orgs.R
+import com.example.orgs.model.Produto
+import com.example.orgs.ui.recyclerview.adapter.ListaProdutosAdapter
+import java.math.BigDecimal
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+//        val  nome = findViewById<TextView>(R.id.nome)
+//        nome.text = "Cesta de frutas"
+//        val descricao = findViewById<TextView>(R.id.descricao)
+//        descricao.text = "Laranja, manga e maçã"
+//        val valor = findViewById<TextView>(R.id.valor)
+//        valor.text = "19.99"
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.adapter = ListaProdutosAdapter(context = this, produtos = listOf(
+            Produto(nome = "teste",
+                    descricao = "teste desc",
+                    valor = BigDecimal("19.99")
+            ),
+            Produto(nome = "teste 1",
+                    descricao = "teste desc 1",
+                    valor = BigDecimal("29.99")
+            ),
+            Produto(nome = "teste 2",
+                    descricao = "teste desc 2",
+                    valor = BigDecimal("39.99")
+            )
+        ))
+//        recyclerView.layoutManager = LinearLayoutManager(this)
+    }
+}
